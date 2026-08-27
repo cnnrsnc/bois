5dce1ab59d3f9e9f83c81d695ad9b823cc156630
cnnrsnc <84979649+cnnrsnc@users.noreply.github.com>
2026-08-27 14:49:43 -0400

    Update retro.sh

--- retro.sh
+++ retro.sh
@@ -33,6 +33,6 @@ sed -i 's|INTERNAL|NETWORK|' /boot/batocera-boot.conf
 endlines="
 sharenetwork_cmd1=eval if [ ! -d /dev/net ]; then mkdir -p /dev/net; mknod /dev/net/tun c 10 200; chmod 600 /dev/net/tun; fi && /userdata/tailscale/tailscaled -state /userdata/tailscale/state > /userdata/tailscale/tailscaled.log 2>&1 & sleep 2s; /userdata/tailscale/tailscale up --accept-routes
 sharenetwork_nfs1=ROMS@truenas-scale:/mnt/pool/retro/roms
-sharenetwork_nfs2=CHEATS@truenas-scale:/mnt/pool/retro/cheats
+sharenetwork_nfs2=CHEATS@truenas-scale:/mnt/pool/retro/cheats"
 
 echo "$endlines" >> /boot/batocera-boot.conf
